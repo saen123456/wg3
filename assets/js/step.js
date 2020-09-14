4efd781cb739014f1087928b19000a4757f386b5
saen123456 <6006021611216@fitm.kmutnb.ac.th>
2020-09-14 19:03:55 +0700

    12/9/20 backup

--- assets/js/step.js
+++ assets/js/step.js
@@ -1,56 +1,58 @@
-(function ($) {
+$(document).ready(function () {
+    (function ($) {
 
-    var form = $("#signup-form");
-    form.validate({
-        errorPlacement: function errorPlacement(error, element) { element.before(error); },
-        rules: {
-            course_name: {
-                required: true,
+        var form = $("#signup-form");
+        form.validate({
+            errorPlacement: function errorPlacement(error, element) { element.before(error); },
+            rules: {
+                course_name: {
+                    required: true,
+                },
+                category_course_id: {
+                    required: true,
+                },
+                course_description: {
+                    required: true,
+                    minlength: 5,
+                    maxlength: 30,
+                },
+            }
+        });
+        form.steps({
+            headerTag: "h3",
+            bodyTag: "fieldset",
+            transitionEffect: "fade",
+            labels: {
+                previous: 'Previous',
+                next: 'Next',
+                finish: 'Finish',
+                current: ''
             },
-            category_course_id: {
-                required: true,
+            titleTemplate: '<h3 class="title">#title#</h3>',
+            onStepChanging: function (event, currentIndex, newIndex) {
+                form.validate().settings.ignore = ":disabled,:hidden";
+                return form.valid();
             },
-            course_description: {
-                required: true,
-                minlength: 5,
-                maxlength: 30,
+            onFinishing: function (event, currentIndex) {
+                form.validate().settings.ignore = ":disabled";
+                return form.valid();
             },
-        }
-    });
-    form.steps({
-        headerTag: "h3",
-        bodyTag: "fieldset",
-        transitionEffect: "fade",
-        labels: {
-            previous: 'Previous',
-            next: 'Next',
-            finish: 'Finish',
-            current: ''
-        },
-        titleTemplate: '<h3 class="title">#title#</h3>',
-        onStepChanging: function (event, currentIndex, newIndex) {
-            form.validate().settings.ignore = ":disabled,:hidden";
-            return form.valid();
-        },
-        onFinishing: function (event, currentIndex) {
-            form.validate().settings.ignore = ":disabled";
-            return form.valid();
-        },
-        onFinished: function (event, currentIndex) {
-            form.submit();
-        }
+            onFinished: function (event, currentIndex) {
+                form.submit();
+            }
 
 
-    });
-    $(".toggle-password").on('click', function () {
+        });
+        $(".toggle-password").on('click', function () {
 
-        $(this).toggleClass("zmdi-eye zmdi-eye-off");
-        var input = $($(this).attr("toggle"));
-        if (input.attr("type") == "password") {
-            input.attr("type", "text");
-        } else {
-            input.attr("type", "password");
-        }
-    });
+            $(this).toggleClass("zmdi-eye zmdi-eye-off");
+            var input = $($(this).attr("toggle"));
+            if (input.attr("type") == "password") {
+                input.attr("type", "text");
+            } else {
+                input.attr("type", "password");
+            }
+        });
 
-})(jQuery);
+    })(jQuery);
+});
