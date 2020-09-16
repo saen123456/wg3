18b1daf61c10f306115fcc6c9ced11496a034b90
saen123456 <6006021611216@fitm.kmutnb.ac.th>
2020-09-16 21:39:59 +0700

    16/9/20 add upload doc and user ans quiz

--- app/Views/Course/Document.php
+++ app/Views/Course/Document.php
@@ -15,9 +15,9 @@
     endforeach;
 
     $document_type = pathinfo($document_name, PATHINFO_EXTENSION);
-    //echo $document_type;
+    //echo $document_link;
     if ($document_type == "doc" || $document_type == "docx" || $document_type == "pdf") {
-        echo "<iframe src='https://docs.google.com/gview?url=" . $document_link . "&embedded=true' width='100%' height='900px' ></iframe>";
+        echo "<iframe src='https://view.officeapps.live.com/op/embed.aspx?src=" . $document_link . "' width='100%' height='900px' frameborder='0'> </iframe>";
     } else {
         $new = str_replace(' ', '%20', $document_link);
         $new = str_replace('&', '%26', $document_link);
